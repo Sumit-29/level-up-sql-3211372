e84f8fbf1006ec66edc782f8e5e5d0dcebdb9968
Sumit Bishnoi <bishnoi680@gmail.com>
2023-08-12 22:12:00 +0000

    Learning SQL

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,6 @@
 -- Generate a list of customer information.
-
+select FirstName, LastName, Email 
+from Customers 
+order by LastName;
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
